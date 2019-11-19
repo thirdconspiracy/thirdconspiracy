8d0f61dbb761f153c2ffd8d59e94d1f765294391
brantley.blanchard <brantley.blanchard@channeladvisor.com>
2019-11-19 08:58:32 -0500

    angular app layout

--- Max/Angular/food/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Max/Angular/food/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pizza Test 1', 'This is simply a test', 'http://normalcooking.com/wp-content/uploads/2014/04/Mini-Biscuit-Pizza-1.jpg'),
+    new Recipe('Pizza Test 2', 'This is simply a test', 'http://normalcooking.com/wp-content/uploads/2014/04/Mini-Biscuit-Pizza-1.jpg'),
+  ];
 
   constructor() { }
 
