b1321a6b2c036c5f43fba9208cbf975c6c0a9893
brantley.blanchard <brantley.blanchard@channeladvisor.com>
2019-12-19 12:51:13 -0500

    stubbing rop function

---
+++ csharp/thirdconspiracy.Utilities/Rop/Result.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace thirdconspiracy.Utilities.Rop
+{
+    public class Result<TSuccess, TFailure>
+    {
+        #region Member Variables
+
+        public bool IsSuccessful { get; set; }
+        public TSuccess Success { get; set; }
+        public TFailure Failure { get; set; }
+
+        #endregion Member Variables
+
+        #region CTOR
+
+        public static Result<TSuccess, TFailure> Succeeded(TSuccess success)
+        {
+            if (success == null)
+            {
+                throw new ArgumentNullException(nameof(success));
+            }
+
+            return new Result<TSuccess, TFailure>
+            {
+                IsSuccessful = true,
+                Success = success
+            };
+        }
+
+        public static Result<TSuccess, TFailure> Failed(TFailure failure)
+        {
+            if (failure == null)
+            {
+                throw new ArgumentNullException(nameof(failure));
+            }
+
+            return new Result<TSuccess, TFailure>
+            {
+                IsSuccessful = false,
+                Failure = failure
+            };
+        }
+
+        private Result()
+        {
+        }
+
+        #endregion CTOR
+
+        public bool IsSuccess => IsSuccessful;
+        public bool IsFailure => !IsSuccessful;
+    }
+}
