008a428c5d09bb9e3740666ee7dad5ca566602bf
blanchardbran <Brantley.Blanchard@garmin.com>
2020-08-27 09:43:04 -0400

    Run tasks as Admin from taskbar

---
+++ EasyLauncher/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace EasyLauncher
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.SuspendLayout();
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+			this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+			this.notifyIcon1.Text = "EasyLauncher";
+			this.notifyIcon1.Visible = true;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+			this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(4, 0);
+			this.ControlBox = false;
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.Text = "Form1";
+			this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+	}
+}
+
---
+++ EasyLauncher/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.IO;
+using System.Threading;
+
+namespace EasyLauncher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class LauncherGroup
+        {
+            public String Name { get; set; }
+            public string Process { get; set; }
+            public List<LauncherEntry> Entries { get; set; }
+            
+        }
+
+        public class LauncherEntry
+		{
+            public string Name { get; set; }
+            public string Process { get; set; }
+            public string Arguments { get; set; }
+
+        }
+
+        private List<LauncherGroup> _launcherGroups;
+
+        private void EnumerateWork(DirectoryInfo di)
+		{
+            var dirFiles = di.GetFiles("*.sln");
+
+            var solutionGroup = _launcherGroups.Where(g => g.Name == "VS Solutions").First();
+
+            foreach (var dirFile in dirFiles)
+            {
+                solutionGroup.Entries.Add(new LauncherEntry
+                {
+                    Name = dirFile.Name,
+                    Arguments = dirFile.FullName
+                });
+            }
+            
+            foreach(var dir in di.GetDirectories())
+			{
+                if (dir.Name.StartsWith("irs-"))
+                    continue;
+
+                EnumerateWork(dir);
+			}
+		}
+
+        private void SaveConfig()
+		{
+            using (var fs = new FileStream("config.json", FileMode.OpenOrCreate, FileAccess.Write, FileShare.ReadWrite))
+            using(var sw = new StreamWriter(fs))
+			{
+                var launcherGroups = Newtonsoft.Json.JsonConvert.SerializeObject(_launcherGroups, Newtonsoft.Json.Formatting.Indented);
+                sw.Write(launcherGroups);
+                sw.Flush();
+			}
+		}
+
+        private void LoadConfig()
+		{
+            using (var fs = new FileStream("config.json", FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+            using (var sr = new StreamReader(fs))
+            {
+                var jsonString = sr.ReadToEnd();
+                _launcherGroups = Newtonsoft.Json.JsonConvert.DeserializeObject<List<LauncherGroup>>(jsonString);
+            }
+        }
+
+        private void BuildConfig()
+		{
+            LoadConfig();
+            foreach(var dir in new DirectoryInfo("c:\\work").GetDirectories("irs-*"))
+                EnumerateWork(dir);
+
+            SaveConfig();
+		}
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+            contextMenuStrip1.Items.Clear();
+            LoadConfig();
+
+            foreach (var group in _launcherGroups)
+            {
+                ToolStripMenuItem item = new ToolStripMenuItem();
+                item.Text = group.Name;
+                item.Tag = group;
+
+                foreach (var entry in group.Entries)
+                {
+                    ToolStripMenuItem menuItem = new ToolStripMenuItem();
+                    menuItem.Text = entry.Name;
+                    menuItem.Tag = entry;
+					menuItem.Click += MenuItem_Click;
+
+                    item.DropDownItems.Add(menuItem);
+                }
+
+                contextMenuStrip1.Items.Add(item);
+            }
+
+            ToolStripMenuItem closeItem = new ToolStripMenuItem();
+            closeItem.Text = "Exit";
+            closeItem.Click += new EventHandler(delegate (object sndr, EventArgs eva) { this.Close(); });
+            contextMenuStrip1.Items.Add(closeItem);
+        }
+
+		private void MenuItem_Click(object sender, EventArgs e)
+		{
+            try
+            {
+                var menu = ((ToolStripMenuItem)sender);
+                var entry = ((LauncherEntry)menu.Tag);
+                var group = ((LauncherGroup)menu.OwnerItem.Tag);
+
+                var processName = group.Process;
+                if (!String.IsNullOrWhiteSpace(entry.Process))
+                {
+                    processName = entry.Process;
+                }
+
+                if(String.IsNullOrWhiteSpace(processName))
+				{
+                    return;
+				}
+
+                var startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.FileName = processName;
+                startInfo.Arguments = entry.Arguments;
+                System.Diagnostics.Process.Start(startInfo);
+            }
+            catch (Exception ex) 
+            {
+                System.Diagnostics.Debug.WriteLine(ex);
+            }
+        }
+
+    }
+}
