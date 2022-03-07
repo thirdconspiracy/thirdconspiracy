19166a674afa09eade087e67c7897ec9af516517
blanchardbran <Brantley.Blanchard@garmin.com>
2022-03-07 12:45:29 -0500

    [master]: Immediate window test

---
+++ csharp/CodeWars.Test/LeetCode/ImmediateWindow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CodeWars.Test.LeetCode;
+
+public class ImmediateWindow
+{
+    [Test]
+    public void OutputStuff()
+    {
+        Console.WriteLine("Hello World");
+        
+        Assert.IsTrue(true);
+    }
+}
