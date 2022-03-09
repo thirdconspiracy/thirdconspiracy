dd8a493d47b879745dcdcb067d5b5b32e274251b
blanchardbran <Brantley.Blanchard@garmin.com>
2022-03-09 08:07:18 -0500

    [master]: leetcode

---
+++ csharp/CodeWars.Test/LeetCode/RegularExpressionMatching.cs
@@ -0,0 +1,82 @@
+﻿using NUnit.Framework;
+
+namespace CodeWars.Test.LeetCode;
+
+public class RegularExpressionMatching
+{
+    [TestCase("aa", "a", false)]
+    [TestCase("aa", "a*", true)]
+    [TestCase("ab", ".*", true)]
+    public void TestCase(string s, string p, bool expected)
+    {
+        var actual = IsArrayMatch(s, p);
+        //var actual = IsRecursiveMatch(s, p);
+        Assert.AreEqual(expected, actual);
+    }
+
+    private bool IsArrayMatch(string s, string p)
+    {
+        var arr = new bool[s.Length + 1, p.Length + 1];
+        arr[0, 0] = true;
+        
+        //will match empty string if '*' represents 0 instances every time
+        var rows = arr.GetLength(0);
+        var col = arr.GetLength(1);
+        
+        for (var i = 1; i < col; i++)
+        {
+            if (p[i - 1] == '*')
+                arr[0, i] = arr[0, i - 2];
+        }
+
+        for (var i = 1; i < rows; i++)
+        {
+            for (var j = 1; j < col; j++)
+            {
+                if (IsCharMatch(s[i - 1], p[j - 1]))
+                    arr[i, j] = arr[i - 1, j - 1];
+                else if (p[j - 1] == '*')
+                {
+                    arr[i, j] = arr[i, j - 2];//Zero
+                    if (!arr[i,j] && IsCharMatch(s[i - 1], p[j - 2]))//Many
+                        arr[i, j] = arr[i - 1, j];
+                }
+                else
+                    arr[i, j] = false;
+            }
+        }
+
+        return arr[s.Length, p.Length];
+    }
+
+    private bool IsRecursiveMatch(string s, string p)
+    {
+        if (p.Length == 0)
+            return s.Length == 0;
+
+        if (p.Length == 1)
+            return s.Length == 1 && IsCharMatch(s[0], p[0]);
+
+        if (p[1] == '*')
+        {
+            //Zero instances
+            if (IsRecursiveMatch(s, p.Substring(2)))
+                return true;
+            //1+ instances
+            return s.Length != 0
+                   && IsCharMatch(s[0], p[0])
+                   && IsRecursiveMatch(s.Substring(1), p);
+        }
+
+        //exact
+        if (s.Length == 0)
+            return false;
+
+        if (p[0] != s[0] && p[0] != '.')
+            return false;
+
+        return IsRecursiveMatch(s.Substring(1), p.Substring(1));
+    }
+
+    private static bool IsCharMatch(char c, char p) => p == c || p == '.';
+}
---
+++ csharp/CodeWars.Test/LeetCode/StringToIntAtoi.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace CodeWars.Test.LeetCode;
+
+public class StringToIntAtoi
+{
+    [TestCase("20000000000000000000", 2147483647)]
+    [TestCase("00000000000000000002", 2)]
+    [TestCase("-20000000000000000000", -2147483648)]
+    [TestCase("-00000000000000000002", -2)]
+    [TestCase("  -0012a42", -12)]
+    [TestCase("  +  413", 0)]
+    [TestCase("-91283472332", -2147483648)]
+    [TestCase("-283472332", -283472332)]
+    [TestCase("21474836460", 2147483647)]
+    public void TestCases(string s, int expected)
+    {
+        var actual = MyAtoi(s);
+        Assert.AreEqual(expected, actual);
+    }
+
+    private HashSet<char> d = new HashSet<char>() { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+    public int MyAtoi(string s)
+    {
+        var token = FirstToken(s);
+        var cleaned = CleanToken(token);
+
+        if (TryConvert(cleaned, out var num))
+            return num;
+        return 0;
+    }
+
+    private IEnumerable<char> FirstToken(string s)
+    {
+        var inToken = false;
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                if (inToken)
+                    yield break;
+                continue;
+            }
+
+            yield return s[i];
+            inToken = true;
+        }
+    }
+
+    private IEnumerable<char> CleanToken(IEnumerable<char> s)
+    {
+        var a = new HashSet<char>() { '.', '+', '-' };
+        var isFirst = true;
+        foreach (var c in s)
+        {
+            if (d.Contains(c))
+                yield return c;
+            else if (a.Contains(c))
+            {
+                if (isFirst)
+                    yield return c;
+                else
+                    yield break;
+            }
+            else
+                yield break;
+
+            isFirst = false;
+        }
+    }
+
+    private bool TryConvert(IEnumerable<char> s, out int num)
+    {
+        num = 0;
+        var sb = new StringBuilder();
+        foreach (var c in s)
+        {
+            sb.Append(c);
+        }
+        var atoi = sb.ToString();
+        
+        if (atoi.Length > 11)
+        {
+            var neg = atoi[0] == '-';
+            for (var i = 0; i < atoi.Length - 11; i++)
+                if (d.Contains(atoi[i]) && atoi[i] != '0')
+                {
+                    num = neg
+                        ? int.MinValue
+                        : int.MaxValue;
+                    return true;
+                }
+            
+            atoi = neg 
+                ? string.Concat("-", atoi.AsSpan(atoi.Length - 11))
+                : atoi.Substring(atoi.Length - 11);
+        }
+        Console.WriteLine(atoi);
+
+        if (long.TryParse(atoi, out var l))
+        {
+            if (l > int.MaxValue)
+                num = int.MaxValue;
+            else if (l < int.MinValue)
+                num = int.MinValue;
+            else
+                num = (int)l;
+            return true;
+        }
+
+        return false;
+    }
+}
